bc18a658c026804774fc153f0d08083dab8fae35
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-09-03 11:03:49 +0200

    Add store example

---
+++ example/store.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math/rand"
+
+	serialized "github.com/marcusolsson/serialized-go"
+	uuid "github.com/satori/go.uuid"
+)
+
+type PaymentProcessed struct {
+	PaymentMethod string `json:"paymentMethod"`
+	Amount        int    `json:"amount"`
+	Currency      string `json:"currency"`
+}
+
+func main() {
+	var (
+		accessKey       = flag.String("access-key", "", "Serialized.io access key")
+		secretAccessKey = flag.String("secret-access-key", "", "Serialized.io secret access key")
+	)
+
+	flag.Parse()
+
+	client := serialized.NewClient(
+		serialized.WithAccessKey(*accessKey),
+		serialized.WithSecretAccessKey(*secretAccessKey),
+	)
+
+	pp := PaymentProcessed{
+		PaymentMethod: "CARD",
+		Amount:        rand.Intn(1000),
+		Currency:      "SEK",
+	}
+
+	err := client.Store("payment", "2c3cf88c-ee88-427e-818a-ab0267511c84", 1,
+		serialized.NewEvent(uuid.NewV4().String(), "PaymentProcessed", pp))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
