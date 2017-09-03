496ad0a26a0a0ebfcdde48e2a1da964d17bad9f0
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-09-03 10:45:34 +0200

    Add feed example

---
+++ example/feed.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	serialized "github.com/marcusolsson/serialized-go"
+)
+
+func main() {
+	var (
+		accessKey       = flag.String("access-key", "", "Serialized.io access key")
+		secretAccessKey = flag.String("secret-access-key", "", "Serialized.io secret access key")
+	)
+
+	flag.Parse()
+
+	client := serialized.NewClient(
+		serialized.WithAccessKey(*accessKey),
+		serialized.WithSecretAccessKey(*secretAccessKey),
+	)
+
+	feed, err := client.Feed("payment")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v", feed.Entries)
+}
